e2e648aaa432ae5d2cfdc502aefe75d92a46dbd6
ISMAIL <isk28954@gmail.com>
2026-07-11 12:02:12 +0530

    OTP testing2

--- email/otp_email.js
+++ email/otp_email.js
@@ -4,14 +4,14 @@ export const sendOtpEmail = async (email, otp) => {
     console.log("Entering Block",otp)
     try {
         return await emailjs.send(
-            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-            process.env.NEXT_PUBLIC_EMAILJS_OTP_TEMPLATE_ID, // Different template
-            {
-                to_email: email,
-                otp: otp,
-                reply_to: process.env.NEXT_PUBLIC_REPLY_EMAIL
-            },
-            process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+            // process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+            // process.env.NEXT_PUBLIC_EMAILJS_OTP_TEMPLATE_ID, // Different template
+            // {
+            //     to_email: email,
+            //     otp: otp,
+            //     reply_to: process.env.NEXT_PUBLIC_REPLY_EMAIL
+            // },
+            // process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
         );
     } catch (error) {
         console.error("OTP email failed:", error);
